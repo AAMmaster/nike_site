deea4d2a2147b6601126895177efeecece55a80b
Ali Asghar <120172954+AAMmaster@users.noreply.github.com>
2024-08-14 15:41:37 +0330

    شمه

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-// let ali = "#E5E5E5"
+// let ali = document.getElementById('ali')
+// let top_middle = document.getElementById('top_midle')
+// ali.addEventListener("change",()=>{
+//     top_middle.style.position =("sticky")
+//     top_middle.style.top =("0px")
+
+// })
+
+
+
+let lastScrollTop = 0;
+const topMidle = document.getElementById('top_midle');
+
+window.addEventListener('scroll', function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        // Scrolling down
+        topMidle.style.top = '-200px'; // Adjust this value as needed
+    } else {
+        // Scrolling up
+        topMidle.style.top = '0';
+    }
+    lastScrollTop = scrollTop;
+});
