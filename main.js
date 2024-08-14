e4d6d438c89b8d038588de2991ac4f2062282db2
Ali Asghar <120172954+AAMmaster@users.noreply.github.com>
2024-08-14 21:08:12 +0330

    a

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
-// let ali = document.getElementById('ali')
-// let top_middle = document.getElementById('top_midle')
-// ali.addEventListener("change",()=>{
-//     top_middle.style.position =("sticky")
-//     top_middle.style.top =("0px")
-
-// })
-
-
+// @media (max-width : 2000px)
 
 let lastScrollTop = 0;
 const topMidle = document.getElementById('top_midle');
 
 window.addEventListener('scroll', function () {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    let scrollTop = document.documentElement.scrollTop;
     if (scrollTop > lastScrollTop) {
         // Scrolling down
         topMidle.style.top = '-200px'; // Adjust this value as needed
