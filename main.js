006902d56d6f89524fb2ae8a610f3997f0b30113
Ali Asghar <120172954+AAMmaster@users.noreply.github.com>
2024-08-19 01:41:27 +0330

    ali

--- main.js
+++ main.js
@@ -15,24 +15,72 @@ window.addEventListener('scroll', function () {
     lastScrollTop = scrollTop;
 });
 
-// const products = document.getElementById("products")
-// let html = ``
-// axios.get('http://localhost:3000/banner')
-// .then((res)=>{
-//     console.log(res.data)
-//     let response = res.data
-//     html += response.map((elem)=>{
-//         return `
-//         <div class="product">
-//             <img src="${elem['poster']}" alt="">
-//             <div class="detail">
-//                 <p>
-//                 ${elem['description']}
-//                 </p>
-//                 <button type="button">shop</button>
-//             </div>
-//         </div>
-//         `
-//     })
-//     products.innerHTML = html
-// })
+const four_picture = document.getElementById("four_picture")
+let html = ``
+axios.get('http://localhost:3000/four_picture')
+    .then((res) => {
+        console.log(res.data)
+        let response = res.data
+        html += response.map((elem) => {
+            return `
+        <div class="picture_${elem.id}">
+
+            <a href=""><img src="${elem[`picture`]}" alt=""></a>
+
+            <div class="details_picture">
+                <p>${elem['title']}</p>
+                <h3>${elem['content']}</h3>
+            </div>
+
+            <div class="shop">
+                <a href="">Shop</a>
+            </div>
+        </div>
+        <div class="picture_${elem.id}">
+            <a href=""><img src="${elem['picture']}" alt=""></a>
+
+            <div class="details_picture">
+                <p>${elem['title']}</p>
+                <h3>${elem['content']}</h3>
+            </div>
+
+            <div class="shop">
+                <a href="">Shop</a>
+            </div>
+        </div>
+        <div class="picture_${elem.id}">
+            <a href=""><img src="${elem['picture']}" alt=""></a>
+
+            <div class="details_picture">
+                <p>${elem['title']}</p>
+                <h3>${elem['content']}</h3>
+            </div>
+
+            <div class="shop">
+                <a href="">Shop</a>
+            </div>
+        </div>
+        <div class="picture_${elem.id}">
+            <a href=""><img src="${elem['picture']}" alt=""></a>
+
+            <div class="details_picture">
+                <p>${elem['title']}</p>
+                <h3>${elem['content']}</h3>
+            </div>
+
+            <div class="shop">
+                <a href="">Shop</a>
+            </div>
+        </div>
+        `
+        }).join(''); // Use join to concatenate the array elements into a single string
+        four_picture.innerHTML = html
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        four_picture.innerHTML = 'Error loading data. Please try again later.';
+    });
+
+
+
+
